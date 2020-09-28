99da94aefb407d47cb8fef94226b04a1ae63d327
Moogii <meka.moogii@gmail.com>
2020-09-28 19:39:41 +0800

    toglogchiin eeljig soligddog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, 1-р тоглогчийг 0, хоёр дугаар тоглогчийг 1 гэж тэмдэглэнэ.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч.
 var scores = [0, 0];
@@ -17,8 +17,46 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+
+// Шоог шидэх эвент листнэр
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 дотор санамсаргүй нэг тоог гаргаж авна.
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // Шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+  // Буусан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
+
+  // Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+
+  if (diceNumber !== 1) {
+    // 1-с ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг  энэ хэсэгт сольж өгнө.
+    roundScore = 0;
+    // Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 00 болгоно.
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // Тоглогчийн ээлжийг нөгөө тоглогчруу шилжүүлнэ.
+    // Хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
+    //Үгүй бол идэвхтэй тоглогчийг 0 болго.
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // Улаан цэгийг шилжүүлэх
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    //Шоог түр алга болгох.
+    diceDom.style.display = "none";
+
+    /*if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    } */
+  }
 });
